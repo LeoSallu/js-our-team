42d66be4c3796dec5c53c5ea942b5b33494f8bae
LeoSallu <L.sallustio99@gmail.com>
2023-03-07 14:43:13 +0100

    Array contenente oggetti completato

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
 'use strict';
 
-// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
-// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
-// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
-// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
-// Scott Estrada	Developer	scott-estrada-developer.jpg
-// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
-
+// Array & Item 
 const arrayTeam = [
     {
         name:'Waine Barnett',
         role:'Founder & CEO',
         img:'wayne-barnett-founder-ceo.jpg' 
+    },
+    {
+        name:'Angela Caroll',
+        role:'Chief Editor',
+        img:'angela-caroll-chief-editor.jpg' 
+    },
+    {
+        name:'Walter Gordon',
+        role:'Office Manager',
+        img:'walter-gordon-office-manager.jpg' 
+    },
+    {
+        name:'Angela Lopez',
+        role:'Social Media Manager',
+        img:'angela-lopez-social-media-manager.jpg' 
+    },
+    {
+        name:'Scott Estrada',
+        role:'Developer',
+        img:'scott-estrada-developer.jpg' 
+    },
+    {
+        name:'Barbara Ramos',
+        role:'Graphic Designer',
+        img:'barbara-ramos-graphic-designer.jpg' 
     }
-]
-console.log(arrayTeam[0]);
+];
+
