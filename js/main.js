de5ddbb431ee0a5fd72778897d6fd71edfa3eae8
LeoSallu <L.sallustio99@gmail.com>
2023-03-07 14:37:42 +0100

    Creazione primo oggetto

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 'use strict';
 
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+const arrayTeam = [
+    {
+        name:'Waine Barnett',
+        role:'Founder & CEO',
+        img:'wayne-barnett-founder-ceo.jpg' 
+    }
+]
+console.log(arrayTeam[0]);
